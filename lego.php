9c87275211c5eb16e0d86149dda8f1818ff017ae
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2021-03-26 23:56:02 -0300

    Add files via upload

---
+++ lego.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class: lego:
+ * function : classe encarregada de montar as partes do motor de template
+ * 
+ */
+namespace kingkernel\suits;
+
+abstract class lego {
+    public function __construct()
+    {
+
+    }
+    public function index()
+    {
+        echo "ola lego class";
+    }
+}
